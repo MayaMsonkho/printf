b96aa3c7516bdebcb5338db9f198a61d316f58d4
MayaMsonkho <markmsonkho@gmail.com>
2022-08-09 12:51:50 -0700

    update

--- main.h
+++ main.h
@@ -1,14 +1,8 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
-
-
 #include <stdarg.h>
-
 #include <stdlib.h>
-
-
 
 /**
  *
@@ -23,47 +17,26 @@
 typedef struct printer
 
 {
-
-		char *specifier;
-
-			int (*run)(va_list);
-
+	char *specifier;
+	int (*run)(va_list);
 } printer;
 
 
-
 printer _get_printer(const char *specifier);
-
 int _putchar(char c);
-
 int _printf(const char *format, ...);
-
 int _print_char(va_list);
-
 int _print_str(va_list);
-
 int _print_decimal(va_list);
-
 int _print_int(va_list);
-
 int _print_binary(va_list);
-
 int _print_str_nonprintable(va_list);
-
 int _print_str_reverse(va_list);
-
 int _print_rot13(va_list);
-
 int _print_uint(va_list);
-
 int _print_octal(va_list);
-
 int _print_hex_lower(va_list);
-
 int _print_hex_upper(va_list);
-
 int _print_pointer(va_list);
 
-
-
-#endif /* MAIN_H */
+#endif
